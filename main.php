3dd2a133ad8d286ab437ef31809c22c9a3d06d47
moh3n <moh3n355@gmail.com>
2025-05-20 15:26:23 +0330

    first commit

---
+++ main.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">    
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>main</title>
+</head>
+<body>
+<form action="/test/arthmetic.php" method="post">
+
+  <fieldset>
+    <legend>شنبه:</legend>
+    <label><input type="radio" name="0shanbe" value="2" required> میتونم بیام</label><br>
+    <label><input type="radio" name="0shanbe" value="1"> ترجیح میدم نیام</label><br>
+    <label><input type="radio" name="0shanbe" value="0"> نمیتونم بیام</label>
+  </fieldset>
+
+  <fieldset>
+    <legend>یکشنبه:</legend>
+    <label><input type="radio" name="1shanbe" value="2" required> میتونم بیام</label><br>
+    <label><input type="radio" name="1shanbe" value="1"> نقدی</label><br>
+    <label><input type="radio" name="1shanbe" value="0"> ارز دیجیتال</label>
+  </fieldset>
+
+  <fieldset>
+    <legend>دوشنبه:</legend>
+    <label><input type="radio" name="2shanbe" value="2" required> میتونم بیام</label><br>
+    <label><input type="radio" name="2shanbe" value="1"> ترجیح میدم نیام</label><br>
+    <label><input type="radio" name="2shanbe" value="0"> نمیتونم بیام</label>
+  </fieldset>
+
+  <fieldset>
+    <legend>سه شنبه:</legend>
+    <label><input type="radio" name="3shanbe" value="2" required> میتونم بیام</label><br>
+    <label><input type="radio" name="3shanbe" value="1"> ترجیح میدم نیام</label><br>
+    <label><input type="radio" name="3shanbe" value="0"> نمیتونم بیام</label>
+  </fieldset>
+
+  <fieldset>
+    <legend>چهار شنبه:</legend>
+    <label><input type="radio" name="4shanbe" value="2" required> میتونم بیام</label><br>
+    <label><input type="radio" name="4shanbe" value="1"> ترجیح میدم نیام</label><br>
+    <label><input type="radio" name="4shanbe" value="0"> نمیتونم بیام</label>
+  </fieldset>
+
+  <fieldset>
+    <legend>پنجشنبه:</legend>
+    <label><input type="radio" name="5shanbe" value="2" required> میتونم بیام</label><br>
+    <label><input type="radio" name="5shanbe" value="1"> ترجیح میدم نیام</label><br>
+    <label><input type="radio" name="5shanbe" value="0"> نمیتونم بیام</label>
+  </fieldset>
+
+  <fieldset>
+    <legend>جمعه:</legend>
+    <label><input type="radio" name="6shanbe" value="2" required> میتونم بیام</label><br>
+    <label><input type="radio" name="6shanbe" value="1"> ترجیح میدم نیام</label><br>
+    <label><input type="radio" name="6shanbe" value="0"> نمیتونم بیام</label>
+  </fieldset>
+  <br>
+  <button type="submit">submit</button>
+</form>
+</body>
+</html>
